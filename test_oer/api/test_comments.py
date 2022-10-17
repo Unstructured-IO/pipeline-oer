5837d35449dd45c5e988824e026112f5875b3ffe
Matt Robinson <mrobinson@unstructuredai.io>
2022-10-17 15:10:02 -0400

    added unit tests

---
+++ test_oer/api/test_comments.py
@@ -0,0 +1,42 @@
+import os
+from pathlib import Path
+import pytest
+
+from fastapi.testclient import TestClient
+
+from prepline_oer.api.comments import app, pipeline_api
+
+DIRECTORY = Path(__file__).absolute().parent
+
+SAMPLE_DOCS_DIRECTORY = os.path.join(DIRECTORY, "..", "..", "sample-docs")
+
+
+@pytest.fixture
+def fake_structured_oer():
+    return {
+        "duty_description": "Personnel and Administration Officer (S1) for a training battalion in the U.S. Army reserve. Principal staff assistant to the battalion commander. Exercise staff supervisor in matters pertaining to strength management, personnel qualifications and evaluations, personnel assignment, clearance, recruiting, retention, and battalion administration. Responsible for the overall supervision of the battalion Personnel Administration Center (PAC) and its activities. Serves as commander of Headquarters and Headquarters Detachment. Additional duties include; Battalion Safety Officer, Equal Opportunity Officer, Records Management Officer, and Retention Officer.",  # noqa: E501
+        "rater_comments": "1LT X performed flawlessly in the execution of an overseas detention and area security mission at Guantanamo Bay, Cuba. Exceptional performance during this limited rating period by CPT X.",  # noqa: E501
+        "character": "1LT X’s exceptional command presence and resilience lends itself to consistent mission accomplishment, good order and discipline, and a positive climate. 1LT X’s outstanding attitude and thirst for knowledge exceeds those around him which contributes to his overall exceptional character.",  # noqa: E501
+        "presence": "1LT X is able to analyze a situation and introduce new ideas when opportunities exist, approaching challenging circumstances with creativity and intellect. 1LT X is highly proficient in interacting with others, effectively adjusting behaviors when interacting with superiors, peers, and subordinates.",  # noqa: E501
+        "intellect": "1LT X demonstrates the full range of required influence techniques enabling him to speak, lead and motivate every person in his unit. 1LT X works with the Alameda County Sheriff’s office, as well as other outside agencies, in order to build positive relationships established that have enhanced unit training.",  # noqa: E501
+        "leads": "Absolute professional and squared away for duty; current on all applicable skills, knowledge, and mental toughness by engaging in engages in continual self-development. Using his extensive experience, 1LT X works well after normal duty hours, provides coaching, and counseling and mentoring.",  # noqa: E501
+        "develops": "Developed AAR reporting template that standardized information across the battalion and ensured compliance with Army Regulations. She consistently presented appropriate and useful monthly reports on security clearances, weather effects, and threat assessments.",  # noqa: E501
+        "achieves": "1LT X is #2 of the 20 Lieutenants I senior rated. He is an asset for the future and will progress further in his military career. Keep assigning him to demanding position and select him for the Military Police Captains Career Course now. Promote ahead of peers to Captain and select him for the next Company Command.",  # noqa: E501
+        "intermediate_rater": "1LT X is #2 of the 20 Lieutenants I senior rated. He is an asset for the future and will progress further in his military career. Keep assigning him to demanding position and select him for the Military Police Captains Career Course now. Promote ahead of peers to Captain and select him for the next Company Command.",  # noqa: E501
+    }
+
+
+def test_pipeline_api(fake_structured_oer):
+    filename = os.path.join(SAMPLE_DOCS_DIRECTORY, "fake-oer.pdf")
+
+    with open(filename, "rb") as f:
+        oer = pipeline_api(f.read())
+
+    assert oer == fake_structured_oer
+
+
+def test_section_narrative_api_health_check():
+    client = TestClient(app)
+    response = client.get("/healthcheck")
+
+    assert response.status_code == 200

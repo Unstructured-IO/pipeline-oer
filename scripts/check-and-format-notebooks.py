feb65971432952cf271342e9a1af9700f2438267
Matt Robinson <mrobinson@unstructuredai.io>
2022-10-18 17:11:15 -0400

    manually clear output

--- scripts/check-and-format-notebooks.py
+++ scripts/check-and-format-notebooks.py
@@ -92,7 +92,9 @@ if __name__ == "__main__":
         modified_nb = deepcopy(nb)
         process_nb(modified_nb, root_path)
         clean.clean_nb(modified_nb, allowed_cell_metadata_keys=["tags"])
-        modified_nb.clear()
+
+        for cell in modified_nb["cells"]:
+            cell["output"] = []
 
         if nb != modified_nb:
             nonmatching_nbs.append(str(fn))
